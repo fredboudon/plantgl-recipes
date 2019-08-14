514a93043b2de10ad15ff4e29c7429ab277ae1f1
Frederic Boudon <frederic.boudon@cirad.fr>
2019-08-14 16:25:00 +0200

    improve build_recipes

--- build_recipes.py
+++ build_recipes.py
@@ -17,12 +17,25 @@ repositories = [
     'pyqglviewer'
 ]
 
-owner = sys.argv[1]
+owner = sys.argv[1] if len(sys.argv) >= 2 else 'openalea'
 branch = sys.argv[2] if len(sys.argv) >= 3 else 'master'
 
 # ------
 # Script
 # ------
+
+
+def execProgram(program, *args):
+    args = [program] + list(args)
+    child = subprocess.run(args, stdout=subprocess.PIPE)
+    #child.communicate()
+    return child.returncode, str(child.stdout)
+
+def execGit(*args):
+    return execProgram('git', *args)
+
+
+eNotExists, eRemoteExists, eBranchExists = 0,1,2
 
 class Repository:
 
@@ -31,30 +44,72 @@ class Repository:
         self.name = name
         self.branch = branch
 
+    def check(self):
+        code = self.checkRemoteRepoExists()
+        if code & eRemoteExists == 0:
+            print('Cannot find asked remote owner of'+repr(self.name)+'. Changing from',repr(self.owner),"to 'openalea'")
+            self.owner = 'openalea'
+        if code & eBranchExists == 0:
+            print('Cannot find asked remote branch of'+repr(self.name)+'. Changing from',repr(self.branch),"to 'master'")
+            self.branch = 'master'
+
+    def checkRemoteRepoExists(self):
+        code, value = execGit('ls-remote', 'https://github.com/' + self.owner + '/' + self.name + '.git', '-b', self.branch)
+        if code != 0 : return eNotExists
+        elif self.branch in value : return eBranchExists | eRemoteExists
+        else : return eRemoteExists
+
+    def update(self):
+        if os.path.exists(self.name) :
+            print('Submodule',self.name,'already exists. Updating')
+        if not os.path.exists(self.name) :
+            self.cloneAsSubmodule()
+        else :
+            if self.localRepoOwner() != self.owner :
+                self.removePreviousSubmodule()
+                self.cloneAsSubmodule()
+            elif self.localRepoBranch() != self.branch:
+                os.chdir(self.name)
+                execGit('checkout','-b',self.branch)
+                execGit('pull')
+                os.chdir(os.pardir)
+            else:
+                os.chdir(self.name)
+                execGit('pull')
+                os.chdir(os.pardir)
+
+
     def cloneAsSubmodule(self):
-        return self.__execGit('submodule', 'add', 'https://github.com/' + self.owner + '/' + self.name + '.git', self.name, '-b', self.branch)
+        return execGit('submodule', 'add', 'https://github.com/' + self.owner + '/' + self.name + '.git', self.name, '-b', self.branch)
 
     def removePreviousSubmodule(self):
         if os.path.exists(self.name):
-            #self.__execGit('submodule', 'deinit', '-f', self.name)
-            #try:
-            #    shutil.rmtree(join('.git', 'modules', self.name))
-            #except:
-            #    pass
-            self.__execGit('rm', '-f', self.name)
+            execGit('rm', '-f', self.name)
+
+    def localRepoOwner(self):
+        remotesignature = 'origin   https://github.com/'
+        if os.path.exists(self.name):
+            os.chdir(self.name)
+            code, value = execGit('remote','-v')
+            os.chdir(os.pardir)
+            if code == 0 and value.startswith(remotesignature):
+                currentowner = value[len(remotesignature):].split('/')[0]
+                return currentowner
+
+    def localRepoBranch(self):
+        if os.path.exists(self.name):
+            os.chdir(self.name)
+            code, value = execGit('branch')
+            os.chdir(os.pardir)
+            for l in value.splitlines():
+                if l.startswith('*'):
+                    return l.split()[1]
 
 
-    def __execGit(self, *args):
-        return self.__execProgram('git', *args)
-
-    def __execProgram(self, program, *args):
-        args = [program] + list(args)
-        child = subprocess.Popen(args)
-        child.communicate()
-        return child.returncode
 
 # Clone submodule
 for repoName in repositories:
     repo = Repository(owner, repoName, branch)
-    repo.removePreviousSubmodule()
-    repo.cloneAsSubmodule()
+    repo.check()
+    repo.update()
+
